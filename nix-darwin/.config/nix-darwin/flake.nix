fe65b4cb4b2f67bb067d760e1889226a306c167f
Mathies <mathies@example.com>
2025-11-16 22:31:53 +0100

    added custom docks items and autohide

--- nix-darwin/.config/nix-darwin/flake.nix
+++ nix-darwin/.config/nix-darwin/flake.nix
@@ -1,102 +1,121 @@
 {
-	description = "mathies nix-darwin config";
+  description = "mathies nix-darwin config";
 
-	inputs = {
-		nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
-		nix-darwin.url = "github:LnL7/nix-darwin";
-		nix-darwin.inputs.nixpkgs.follows = "nixpkgs";
-		nix-homebrew.url = "github:zhaofengli/nix-homebrew";
-	};
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    nix-darwin.url = "github:LnL7/nix-darwin";
+    nix-darwin.inputs.nixpkgs.follows = "nixpkgs";
+    nix-homebrew.url = "github:zhaofengli/nix-homebrew";
+  };
 
-	outputs = { self, nix-darwin, nixpkgs, nix-homebrew }: {
-		darwinConfigurations."mathies-macos" = nix-darwin.lib.darwinSystem {
-			system = "aarch64-darwin";
-			modules = [
-				nix-homebrew.darwinModules.nix-homebrew
-				{
-					nix-homebrew = {
-						enable = true;
-						enableRosetta = false;
-						user = "mathies";
-					};
-				}
-				({ pkgs, ... }: {
-					environment.systemPackages = with pkgs; [
-						neovim
-						git
-						stow
-						zsh-powerlevel10k
-						tree
+  outputs = { self, nix-darwin, nixpkgs, nix-homebrew }: {
+    darwinConfigurations."mathies-macos" = nix-darwin.lib.darwinSystem {
+      system = "aarch64-darwin";
+      modules = [
+        nix-homebrew.darwinModules.nix-homebrew
+        {
+          nix-homebrew = {
+            enable = true;
+            enableRosetta = false;
+            user = "mathies";
+          };
+        }
+      ({ pkgs, ... }: {
+       environment.systemPackages = with pkgs; [
+       neovim
+       git
+       stow
+       zsh-powerlevel10k
+       tree
 
-						# for neovim
-						ripgrep
-						fd
-						gcc
-						nodejs_20
-						lazygit
-						fzf
-					];					
-				
-				homebrew = {
-					enable = true;
+# for neovim
+       ripgrep
+       fd
+       gcc
+       nodejs_20
+       lazygit
+       fzf
+       ];					
 
-					casks = [
-						"ghostty"
-						"zoom"
-						"font-meslo-lg-nerd-font"
-					];
+       homebrew = {
+       enable = true;
 
-					onActivation = {
-						cleanup = "zap";
-						autoUpdate = true;
-						upgrade = true;
-					};
-				};
+       casks = [
+         "ghostty"
+           "zoom"
+           "font-meslo-lg-nerd-font"
+       ];
 
-				
-					nix.settings.experimental-features = "nix-command flakes";
-					programs.zsh.enable = true;
-					programs.zsh = {
-						promptInit = ''
-						source ${pkgs.zsh-powerlevel10k}/share/zsh-powerlevel10k/powerlevel10k.zsh-theme
-							'';
-					};
+       onActivation = {
+         cleanup = "zap";
+         autoUpdate = true;
+         upgrade = true;
+       };
+       };
+
+       system.defaults = {
+         dock = {
+           autohide = true;
+           show-recents = false;
+           tilesize = 48;
+
+           persistent-apps = [
+               "/Applications/Ghostty.app"
+               "/System/Cryptexes/App/System/Applications/Safari.app"
+               "/System/Applications/Mail.app"
+               "/System/Applications/Calendar.app"
+           ];
+
+           persistent-others = [
+             "/Users/mathies/Downloads"
+           ];
+         };
+       };
+
+
+       nix.settings.experimental-features = "nix-command flakes";
+       programs.zsh.enable = true;
+       programs.zsh = {
+         promptInit = ''
+           source ${pkgs.zsh-powerlevel10k}/share/zsh-powerlevel10k/powerlevel10k.zsh-theme
+           '';
+       };
 
 
 
-					system.stateVersion = 5;
-					system.primaryUser = "mathies";
+       system.stateVersion = 5;
+       system.primaryUser = "mathies";
 
-					system.keyboard = {
-						enableKeyMapping = true;
-						remapCapsLockToControl = true;
-					};
+       system.keyboard = {
+         enableKeyMapping = true;
+         remapCapsLockToControl = true;
+       };
 
-					system.activationScripts.extraActivation.text = ''
+       system.activationScripts.extraActivation.text = ''
 # Apply settings immediately
-						/System/Library/PrivateFrameworks/SystemAdministration.framework/Resources/activateSettings -u
+         /System/Library/PrivateFrameworks/SystemAdministration.framework/Resources/activateSettings -u
 
 # Run setup script as user (we're root, so use sudo -u)
-						echo "Running default terminal setup..."
-						/usr/bin/sudo -u mathies /usr/bin/env HOME=/Users/mathies /bin/bash ${./set-default-terminal.sh}
+         echo "Running default terminal setup..."
+         /usr/bin/sudo -u mathies /usr/bin/env HOME=/Users/mathies /bin/bash ${./set-default-terminal.sh}
 
 
 # Setup SSH key
-					echo "Checking SSH setup.."
-						/usr/bin/sudo -u mathies /usr/bin/env HOME=/Users/mathies /bin/bash ${./setup-ssh.sh}
+       echo "Checking SSH setup.."
+         /usr/bin/sudo -u mathies /usr/bin/env HOME=/Users/mathies /bin/bash ${./setup-ssh.sh}
 
-					'';
-
-					
-					nixpkgs.hostPlatform = "aarch64-darwin";
+       '';
 
 
-					users.users.mathies = {
-						name = "mathies";
-						home = "/Users/mathies";
-					};
-				})	
-			];
-		};
-	};
+       nixpkgs.hostPlatform = "aarch64-darwin";
+
+
+       users.users.mathies = {
+         name = "mathies";
+         home = "/Users/mathies";
+       };
+      })	
+      ];
+    };
+  };
 }
