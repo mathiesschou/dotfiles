e73a0ea185a9267c89c9602e99b5e82f9d9046cf
Mathies <mathies@example.com>
2025-11-16 21:42:08 +0100

    powerlevel10k and nerdfont

--- nix-darwin/.config/nix-darwin/flake.nix
+++ nix-darwin/.config/nix-darwin/flake.nix
@@ -25,6 +25,7 @@
 						neovim
 						git
 						stow
+						zsh-powerlevel10k
 					];					
 				
 				homebrew = {
@@ -32,6 +33,8 @@
 
 					casks = [
 						"ghostty"
+						"zoom"
+						"font-meslo-lg-nerd-font"
 					];
 
 					onActivation = {
@@ -41,25 +44,39 @@
 					};
 				};
 
+				
 					nix.settings.experimental-features = "nix-command flakes";
 					programs.zsh.enable = true;
+					programs.zsh = {
+						promptInit = ''
+						source ${pkgs.zsh-powerlevel10k}/share/zsh-powerlevel10k/powerlevel10k.zsh-theme
+							'';
+					};
+
+
 
 					system.stateVersion = 5;
 					system.primaryUser = "mathies";
-system.activationScripts.extraActivation.text = ''
-  # Apply settings immediately
-  /System/Library/PrivateFrameworks/SystemAdministration.framework/Resources/activateSettings -u
-  
-  # Run setup script as user (we're root, so use sudo -u)
-  echo "Running default terminal setup..."
-  /usr/bin/sudo -u mathies /usr/bin/env HOME=/Users/mathies /bin/bash ${./set-default-terminal.sh}
+
+					system.keyboard = {
+						enableKeyMapping = true;
+						remapCapsLockToControl = true;
+					};
+
+					system.activationScripts.extraActivation.text = ''
+# Apply settings immediately
+						/System/Library/PrivateFrameworks/SystemAdministration.framework/Resources/activateSettings -u
+
+# Run setup script as user (we're root, so use sudo -u)
+						echo "Running default terminal setup..."
+						/usr/bin/sudo -u mathies /usr/bin/env HOME=/Users/mathies /bin/bash ${./set-default-terminal.sh}
 
 
-  # Setup SSH key
-  echo "Checking SSH setup.."
-  /usr/bin/sudo -u mathies /usr/bin/env HOME=/Users/mathies /bin/bash ${./setup-ssh.sh}
+# Setup SSH key
+					echo "Checking SSH setup.."
+						/usr/bin/sudo -u mathies /usr/bin/env HOME=/Users/mathies /bin/bash ${./setup-ssh.sh}
 
-'';
+					'';
 
 					
 					nixpkgs.hostPlatform = "aarch64-darwin";
