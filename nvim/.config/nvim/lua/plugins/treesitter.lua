e5edca8db4a60ecaa0608010e1bca96de4287ec8
mathiesschou <mathiesschou@icloud.com>
2025-12-16 08:48:37 +0100

    nvim

--- nvim/.config/nvim/lua/plugins/treesitter.lua
+++ nvim/.config/nvim/lua/plugins/treesitter.lua
@@ -2,7 +2,6 @@ return {
   {
     "nvim-treesitter/nvim-treesitter",
     branch = "main",
-    build = ":TSUpdate",
     event = { "BufReadPost", "BufNewFile" },
     config = function()
       -- Enable highlighting per buffer via autocmd
