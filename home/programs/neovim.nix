e88cc13b659fc2825b9dbdbd94ebaa37a7f687c2
mathiesschou <mathiesschou@icloud.com>
2026-01-19 23:20:05 +0100

    remove nvim wrapper, add neovim-qt (gvim alternative)

--- home/programs/neovim.nix
+++ home/programs/neovim.nix
@@ -1,39 +1,17 @@
 { pkgs, ... }:
 
 {
-  programs.neovim = {
-    enable = true;
-    defaultEditor = true;
-    viAlias = true;
-    vimAlias = true;
-    plugins = with pkgs.vimPlugins; [
-      (nvim-treesitter.withPlugins (p: [
-        p.lua
-        p.c
-        p.cpp
-        p.javascript
-        p.typescript
-        p.tsx
-        p.html
-        p.css
-        p.json
-        p.python
-        p.rust
-        p.nix
-        p.latex
-        p.typst
-        p.svelte
-        p.bash
-        p.markdown
-        p.markdown_inline
-        p.vim
-        p.vimdoc
-      ]))
-    ];
+  # Install neovim directly without wrapper
+  home.packages = with pkgs; [
+    neovim
+    neovim-qt  # gvim alternative
+  ];
+
+  home.sessionVariables = {
+    EDITOR = "nvim";
   };
 
   # Link neovim configuration directory
-  # This allows you to keep your existing nvim config structure
   home.file.".config/nvim" = {
     source = ../../nvim/.config/nvim;
     recursive = true;
