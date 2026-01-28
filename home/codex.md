0f7199e25be294bef532234bdd7cbbd7817b6667
mathies <mathiesschou@icloud.com>
2026-01-28 20:30:22 +0100

    add app store numbers to darwin

--- home/codex.md
+++
@@ -1,1 +0,0 @@
-codex resume 019c0466-5f6f-7231-b40a-2bf40df55f86
--- system/homebrew.nix
+++ system/homebrew.nix
@@ -32,6 +32,7 @@
     masApps = {
       "Things 3" = 904280696;
       "Flow" = 1423210932;
+      "Numbers" = 409203825;
     };
 
     onActivation = {
